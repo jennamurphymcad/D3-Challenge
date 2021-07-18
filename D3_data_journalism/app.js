136285518f12e414de9ae3dca204ea762dc81189
Jenna Murphy <jennadeemurphy@gmail.com>
2021-07-17 23:24:04 -0500

    figured out how to get text labels on all circles and not just some

--- D3_data_journalism/app.js
+++ D3_data_journalism/app.js
@@ -1,19 +1,20 @@
 // @TODO: YOUR CODE HERE!
 // Define SVG area dimensions
 var svgWidth = 825;
-var svgHeight = 500;
+var svgHeight = 600;
 
 // Define the chart's margins as an object
 var margin = {
-  top: 60,
-  right: 60,
-  bottom: 60,
-  left: 60
+  top: 100,
+  right: 100,
+  bottom: 100,
+  left: 100
 };
 
 // Define dimensions of the chart area
 var chartWidth = svgWidth - margin.left - margin.right;
 var chartHeight = svgHeight - margin.top - margin.bottom;
+
 
 // Select body, append SVG area to it, and set its dimensions
 var svg = d3.select("#scatter")
@@ -31,33 +32,27 @@ var chartGroup = svg.append("g")
 d3.csv("data.csv", function(d) {
   d.age = +d.age;
   d.smokes = +d.smokes;
+  // d.abbr = +d.abbr;
   return d;
 }).then(function(csvData) {
 
   console.log(csvData);
-    // for (var i = 0; i < csvData.length; i++) {
-    //     console.log(csvData[i].state);
-    //     console.log(csvData[i].abbr);
-    // };
-    //   Step 1: Parse Data/Cast as numbers
-    // // ==============================
-    // Step 2: Create scale functions
-    // ==============================
+
+
+    // Create scale functions
     var xLinearScale = d3.scaleLinear()
-      .domain([30, d3.max(csvData, d => d.age)])
+      .domain([30, d3.max(csvData, d => d.age)]).nice()
       .range([0, chartWidth]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(csvData, d => d.smokes)])
+      .domain([6, d3.max(csvData, d => d.smokes)], 1).nice()
       .range([chartHeight, 0]);
 
-    // Step 3: Create axis functions
-    // ==============================
+    // Create axis functions
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
 
-    // Step 4: Append Axes to the chart
-    // ==============================
+    // Append Axes to the chart
     chartGroup.append("g")
       .attr("transform", `translate(0, ${chartHeight})`)
       .call(bottomAxis);
@@ -65,56 +60,80 @@ d3.csv("data.csv", function(d) {
     chartGroup.append("g")
       .call(leftAxis);
 
-    // Step 5: Create Circles
-    // ==============================
+
+    // Create Circles
     var circlesGroup = chartGroup.selectAll("circle")
     .data(csvData)
     .enter()
     .append("circle")
+    .classed("stateCircle", true)
     .attr("cx", d => xLinearScale(d.age))
     .attr("cy", d => yLinearScale(d.smokes))
     // .attr("class", "stateCircle");
-    .attr("r", "15")
-    .attr("fill", "blue")
-    .attr("opacity", ".5");
+    .attr("r", "10")
+    // .attr("class", ".stateCircle");
+    // .attr("fill", "blue")
+    .attr("opacity", ".8");
 
-    // Step 6: Initialize tool tip
-    // ==============================
+    // create State Abbreviation labels
+    var textGroup = chartGroup.selectAll(".label")
+       .data(csvData)
+       .enter()
+       .append("text")
+       .classed("stateText", true)
+       .classed("label", true)
+       .html(function(d){ return (`${d.abbr}`)})
+        .attr("x", d =>  xLinearScale(d.age))
+        .attr("y", d =>  yLinearScale(d.smokes)+3)
+        .attr("r", "5");;
+
+    
+      // Initialize tool tip
     var toolTip = d3.tip()
-      .attr("class", ".stateText")
-      .attr("class", ".d3-tip")
       .offset([80, -60])
       .html(function(d) {
-        return (`${d.abbr}<br>Age: ${d.age}<br>Smokes: ${d.smokes}`);
-      });
+        return (`state: ${d.abbr}<br>Age: ${d.age}<br>Smokes: ${d.smokes}`);
+      })
+      .attr("class", "d3-tip")
+      .style("z-index", "10");
 
-    // Step 7: Create tooltip in the chart
-    // ==============================
+
+
+    // Create tooltip in the chart
     chartGroup.call(toolTip);
 
-    // Step 8: Create event listeners to display and hide the tooltip
-    // ==============================
+    // Create event listeners to display and hide the tooltip
     circlesGroup.on("click", function(data) {
-      toolTip.show(data, this);
+      toolTip.show(data, this)   
     })
       // onmouseout event
-      .on("mouseout", function(data, index) {
+      .on("mouseout", function(data) {
         toolTip.hide(data);
       });
+
+    // create event listeners to dispaly and hide tooltip if user clicks on abbreviation text also
+    textGroup.on("click", function(data) {
+      toolTip.show(data, this)       
+    })
+        // onmouseout event
+      .on("mouseout", function(data) {
+        toolTip.hide(data);
+      });
+
 
     // Create axes labels
     chartGroup.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 0 - margin.left + 40)
-      .attr("x", 0 - (height / 2))
+      .attr("x", 0 - (chartHeight / 2))
       .attr("dy", "1em")
       .attr("class", "axisText")
-      .text("Number of Billboard 100 Hits");
+      .text("Smokers (%)");
 
     chartGroup.append("text")
-      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + margin.top - 50})`)
       .attr("class", "axisText")
-      .text("Hair Metal Band Hair Length (inches)");
+      .text("Age");
   }).catch(function(error) {
     console.log(error);
   });
