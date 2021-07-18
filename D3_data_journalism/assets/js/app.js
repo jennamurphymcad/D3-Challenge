29d0bada5f4185995b6a2a9da21402f77424e14e
Jenna Murphy <jennadeemurphy@gmail.com>
2021-07-17 19:59:50 -0500

    got plot working

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,22 @@
 // @TODO: YOUR CODE HERE!
+// Load data from data csv
+d3.csv("./data/data.csv").then(function(csvData) {
+
+    console.log(csvData);
+  
+    // log a list of names
+//     var names = csvData.map(data => data.name);
+//     console.log("names", names);
+  
+//     // Cast each hours value in tvData as a number using the unary + operator
+//     tvData.forEach(function(data) {
+//       data.hours = +data.hours;
+//       //data.hours = parseInt(data.hours);
+//       //same as above
+//       console.log("Name:", data.name);
+//       console.log("Hours:", data.hours);
+//     });
+//   }).catch(function(error) {
+//     console.log(error);
+  });
+  
